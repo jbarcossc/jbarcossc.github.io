e5c371e0d1f59faa90ba8fad7c8fe5390dd1baff
jbarcossc <juancruzbarcos@hotmail.com>
2022-07-08 18:19:47 -0300

    Text miss

--- gameOfLife/language.js
+++ gameOfLife/language.js
@@ -37,7 +37,7 @@ let languages = {
         "explanation-1.5" : " en 1970. Es un juego de cero jugadores, en el que su evolución es determinada por un estado inicial, sin requerir intervención adicional.",
         "rules-0" : "Reglas",
         "rules-1" : 'Se trata de un juego de cero jugadores, lo que quiere decir que su evolución está determinada por el estado inicial y no necesita ninguna entrada de datos posterior. El "tablero de juego" es una malla plana formada por cuadrados (las "células") que se extiende por el infinito en todas las direcciones. Por tanto, cada célula tiene 8 células "vecinas", que son las que están próximas a ella, incluidas las diagonales. Las células tienen dos estados: están "vivas" o "muertas" (o "encendidas" y "apagadas"). El estado de las células evoluciona a lo largo de unidades de tiempo discretas (se podría decir que por turnos). El estado de todas las células se tiene en cuenta para calcular el estado de las mismas al turno siguiente. Todas las células se actualizan simultáneamente en cada turno, siguiendo estas reglas:',
-        "rule-1" : "1. Una célula viva con menos de dos vecinos vivos muere, por baja población",
+        "rule-1" : "1. Una célula viva con menos de dos vecinos vivos muere, por baja población.",
         "rule-2" : "2. Una célula viva con dos o tres vecinos vivos vive en la próxima generación.",
         "rule-3" : "3. Una célula viva con más de tres vecinos vivos muere, por sobrepoblación.",
         "rule-4" : "4. Una célula muerta con exactamente tres vecinos vivos se transforma en una célula viva, por reproducción.",
